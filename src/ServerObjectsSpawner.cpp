5de63e0bd2c65e5f74aae783787d7d6190eba916
Roman Hajdúk <xhajduk@fi.muni.cz>
2017-05-07 15:29:06 +0200

    changed collider to rectangle

--- src/ServerObjectsSpawner.cpp
+++ src/ServerObjectsSpawner.cpp
@@ -8,8 +8,13 @@ Id spawnPlayer_Server(EngineType_Server &gameEngine) {
     Id id = gameEngine.spawnGameObject("player");
     gameEngine.addComponent<Transform>(id);
     gameEngine.addComponent<NetworkId>(id);
-    gameEngine.addComponent<CircleCollider>(id, ServerGameConstants::kPlayerColliderCenter, ServerGameConstants::kPlayerColliderRadius,
-    ServerGameConstants::kPlayerCollisionLayer);
+    //gameEngine.addComponent<CircleCollider>(id, ServerGameConstants::kPlayerColliderCenter, ServerGameConstants::kPlayerColliderRadius,
+    //ServerGameConstants::kPlayerCollisionLayer);
+
+    Vector_Float offset{-32, -32};
+    Vector_Float offset2{32, 32};
+    gameEngine.addComponent<RectangleCollider>(id, offset, offset2, ServerGameConstants::kPlayerCollisionLayer);
+
     gameEngine.addComponent<RigidBody>(id, ServerGameConstants::kPlayerInverseMass, ServerGameConstants::kPlayerRestitution,
                                        ServerGameConstants::kPlayerSpeedDecrement);
     gameEngine.addComponent<PlayerComp>(id);
